054d99a4eaf957217fc8eafd809798403179e98b
Waris-Raza <mraza.mwaris@gmail.com>
2022-03-13 14:34:30 +0530

    dt

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:students_adda/constants.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget  is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
